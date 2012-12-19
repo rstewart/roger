d4e4ff8411d4e32f88e2e6d5674c0411ef2c8fbb
rstewart <rstewart@shopwiki.com>
2012-12-19 17:32:15 -0500

    WHAT: Add comments to the ResponseStatus enum

--- src/com/shopwiki/messaging/rpc/ResponseStatus.java
+++ src/com/shopwiki/messaging/rpc/ResponseStatus.java
@@ -5,9 +5,10 @@ package com.shopwiki.messaging.rpc;
  */
 public enum ResponseStatus {
     OK,
-    MALFORMED_REQUEST,
-    INVALID_REQUEST,
-    HANDLER_ERROR,
+    MALFORMED_REQUEST, // An Exception was thrown while attempting deserialize the request from JSON into a Java Object
+    INVALID_REQUEST, // The RequestHandler threw an IllegalArgumentException
+    HANDLER_ERROR, // The RequestHandler threw an Exception
+
     // never returned, only for logging
     NACK,
     NUCLEAR,
